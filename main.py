1e18de5b62d4502635fc834b1ecfa029ab25211e
Kate-Tol3 <95471024+Kate-Tol3@users.noreply.github.com>
2024-04-20 22:09:06 +0300

    second attemp to change

--- main.py
+++ main.py
@@ -4,35 +4,42 @@ TOKEN = '6800574446:AAG1B5NSyjiX8Y84WbKACBNW6b6VYQyVgpk'
 
 #!/usr/bin/python
 
+users = {}
+
+def printf(func):
+    async def wrapper(msg):
+        global users
+        await func(msg)
+        print(users)
+    return wrapper
+
 from telebot.async_telebot import AsyncTeleBot
 bot = AsyncTeleBot(TOKEN)
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
+@printf
 async def send_welcome(msg):
+    global users
     welcome_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_add_note = telebot.types.KeyboardButton(text = "Добавить заметку")
     button_delete_note = telebot.types.KeyboardButton(text = "Удалить заметку")
     button_show_list_note = telebot.types.KeyboardButton(text = "Показать список заметок")
     button_special_keyboard = telebot.types.KeyboardButton(text = "Особые возможности")
     welcome_keyboard.add(button_add_note, button_delete_note, button_show_list_note, button_special_keyboard)
-    await bot.send_message(msg.chat.id,"HI?", reply_markup=welcome_keyboard )
-    state[msg.from_user.id] = 'selecting_action'
+    await bot.send_message(msg.chat.id,"HI?", reply_markup=welcome_keyboard)
+    users[msg.from_user.id] = {'status': 'selecting_action'}
 
 
 
-def printf(func):
-    async def wrapper(msg):
-        global state
-        await func(msg)
-        print(state)
-    return wrapper
+
+
 
 
 # from transitions import Machine
 #
-# class MyStates:
-#     states = ['start', 'selecting_action','making_changes', 'saving_changes', 'back_to menu']
+# class Myuserss:
+#     userss = ['start', 'selecting_action','making_changes', 'saving_changes', 'back_to menu']
 #     transitions = [
 #         {'trigger': 'select_action', 'source': 'start', 'dest': 'selecting_action'},
 #         {'trigger': 'add_note', 'source': 'selecting_action', 'dest': 'making_changes'},
@@ -43,12 +50,13 @@ def printf(func):
 #     ]
 #
 #     def __init__(self):
-#         self.machine = Machine(model=self, states=MyStates.states, transitions=MyStates.transitions, initial='start')
-
-state = {}
+#         self.machine = Machine(model=self, userss=Myuserss.userss, transitions=Myuserss.transitions, initial='start')
 
 
-@bot.message_handler(func=lambda msg: msg.text == 'Особые возможности' and state[msg.from_user.id] == 'selecting_action' if state[msg.from_user.id] else False)
+#users[msg.from_user.id] = {'status': 'selecting_action', 'data': {}}
+
+
+@bot.message_handler(func=lambda msg: msg.text == 'Особые возможности' and users[msg.from_user.id]== {'status':'selecting_action'} if users[msg.from_user.id] else False)
 @printf
 async def show_special_keybrd(msg):
     special_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -60,54 +68,54 @@ async def show_special_keybrd(msg):
     await bot.send_message(msg.chat.id,"a", reply_markup=special_keyboard)
 
 
-@bot.message_handler(func=lambda msg: msg.text == 'Добавить заметку' and state[msg.from_user.id] == 'selecting_action' if state[msg.from_user.id] else False)
+@bot.message_handler(func=lambda msg: msg.text == 'Добавить заметку' and users[msg.from_user.id]['status'] == 'selecting_action' if users[msg.from_user.id] else False)
 @printf
 async def add_note(msg):
-    global state
-    state[msg.from_user.id] = 'adding_note'
-    #user_state.add_note()
-    await bot.send_message(msg.chat.id, "Введите заметку" )#if state[msg.from_user.id] else False)
-    #state[msg.from_user.id] = 'selecting_action'
+    global users
+    users[msg.from_user.id] = {'status': 'adding_note'}
+    #user_users.add_note()
+    await bot.send_message(msg.chat.id, "Введите заметку" )#if users[msg.from_user.id] else False)
+    #users[msg.from_user.id] = 'selecting_action'
 
 
-@bot.message_handler(func=lambda msg: msg.text == 'Удалить заметку' and state[msg.from_user.id] == 'selecting_action' if state[msg.from_user.id] else False)
+@bot.message_handler(func=lambda msg: msg.text == 'Удалить заметку' and users[msg.from_user.id]['status'] == 'selecting_action' if users[msg.from_user.id] else False)
 @printf
 async def delete_note(msg):
-    global state
-    state[msg.from_user.id] = 'deleting_note'
-    #user_state.delete_note()
+    global users
+    users[msg.from_user.id] = {'status': 'deleting_note'}
+    #user_users.delete_note()
     await bot.send_message(msg.chat.id, "Выберите заметку")
-    state[msg.from_user.id] = 'selecting_action'
+    users[msg.from_user.id] = {'status': 'selecting_action'}
 
 
-@bot.message_handler(func=lambda msg: msg.text == 'Показать список заметок' and state[msg.from_user.id] == 'selecting_action' if state[msg.from_user.id] else False)
+@bot.message_handler(func=lambda msg: msg.text == 'Показать список заметок' and users[msg.from_user.id]['status'] == 'selecting_action' if users[msg.from_user.id] else False)
 @printf
 async def show_list_note(msg):
-    global state
-    state[msg.from_user.id] = 'showing_list_notes'
-    #user_state.show_list_note_note()
+    global users
+    users[msg.from_user.id]['status'] = 'showing_list_notes'
+    #user_users.show_list_note_note()
     await bot.send_message(msg.chat.id, "Список заметок")
-    state[msg.from_user.id] = 'selecting_action'
+    users[msg.from_user.id]['status'] = 'selecting_action'
 
 
-@bot.message_handler(func=lambda msg: msg.text == 'Вернуться в начало'and state[msg.from_user.id] == 'selecting_action' if state[msg.from_user.id] else False)
+@bot.message_handler(func=lambda msg: msg.text == 'Вернуться в начало'and users[msg.from_user.id]['status'] == 'selecting_action' if users[msg.from_user.id] else False)
 @printf
 async def go_back (msg):
-    global state
-    state[msg.from_user.id] = 'going_back'
-    #user_state.go_back()
+    global users
+    users[msg.from_user.id]['status'] = 'going_back'
+    #user_users.go_back()
     await bot.send_message(msg.chat.id, "/start")
-    state[msg.from_user.id] = 'selecting_action'
+    users[msg.from_user.id]['status'] = 'selecting_action'
 
 
 
 #adding_notes
 
-@bot.message_handler(func=lambda msg: state[msg.from_user.id] == 'adding_note' if state[msg.from_user.id] else False)
+@bot.message_handler(func=lambda msg: users[msg.from_user.id]['status'] == 'adding_note' if users[msg.from_user.id] else False)
 @printf
 async def add_note(msg):
-    global state
-    state[msg.from_user.id] = 'making_note'
+    global users
+    users[msg.from_user.id]['status'] = 'making_note'
 
     ans_keyboard = telebot.types.InlineKeyboardMarkup()
     button_save = telebot.types.InlineKeyboardButton(text="Сохранить",
@@ -122,35 +130,35 @@ async def add_note(msg):
 @bot.callback_query_handler(func=lambda call: call.data == 'save_note')
 @printf
 async def save_btn(call):
-    global state
+    global users
     message = call.message
     chat_id = message.chat.id
-    state[message.from_user.id] = 'saving_note'
+    users[call.from_user.id]['status'] = 'saving_note'
     await bot.send_message(chat_id, f'Данные сохранены')
-    state[message.from_user.id] = 'selecting_action'
+    users[call.from_user.id]['status'] = 'selecting_action'
     print(call)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'change_note')
 @printf
 async def change_btn(call):
-    global state
+    global users
     message = call.message
     chat_id = message.chat.id
     await bot.send_message(chat_id, f'Введите новую заметку')
-    state[call.from_user.id] = 'adding_note'
+    users[call.from_user.id]['status'] = 'adding_note'
     print(call)
 
 
 
-# @bot.message_handler(func=lambda msg: state[msg.from_user.id] == 'saving_note' if state[msg.from_user.id] else False)
+# @bot.message_handler(func=lambda msg: users[msg.from_user.id] == 'saving_note' if users[msg.from_user.id] else False)
 # async def save_note(msg):
-#     global state
+#     global users
 #     if msg.text == "yes":
 #         await bot.send_message(msg.chat.id, "Заметка сохранена")
 #     else:
 #         await bot.send_message(msg.chat.id, "Заметка не сохранена")
-#     state[msg.from_user.id] = 'selecting_action'
+#     users[msg.from_user.id] = 'selecting_action'
 
 
 
@@ -169,19 +177,19 @@ async def change_btn(call):
 # # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 
 
-# state = { }
+# users= { }
 #
-# @bot.message_handlder(func = lambda x: state[user_id]["await_after_button"] if state[user_id] else False)
+# @bot.message_handlder(func = lambda x: users[user_id]["await_after_button"] if users[user_id] else False)
 # def do_something_after_button(msg):
-#     global state
-#     state[msg.from_user.id]["await_after_button"] = False
+#     global users
+#     users[msg.from_user.id]["await_after_button"] = False
 #     print("хаха, оно работает")
 #
 #
 # @bot.message_handler(regex=r"Моя кнопка")
 # def on_button(msg):
-#     global state
-#     state[msg.from_user.id]["await_after_button"] = True
+#     global users
+#     users[msg.from_user.id]["await_after_button"] = True
 
 import asyncio
 asyncio.run(bot.polling())
